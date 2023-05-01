782bd75de31fedbf6311aaa3b82aa79bef52e05c
SashaSlad <sashachebyrasha16@gmail.com>
2023-05-01 17:31:48 +0300

    restart

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import h from './Header.module.css';
 
 const Header = (props) => {
@@ -7,10 +7,7 @@ const Header = (props) => {
 		<header className={h.header}>
 			<img alt='' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4Qz3nO_Sojnkbp0PVDRmmPVtwMtm7l5usww&usqp=CAU' />
 			<div className={h.login_block}>
-				{/* {props.isAuth ? <div> {props.login} - <button onClick={props.logout}>Log Out</button></div> : <NavLink to={'/login'}>Login</NavLink >} */}
-				{/* Забрав логін для резюме */}
-				<button>Log Out</button>
-
+				{props.isAuth ? <div> {props.login} - <button onClick={props.logout}>Log Out</button></div> : <NavLink to={'/login'}>Login</NavLink >}
 			</div>
 		</header >
 	)
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,77 +1,77 @@
-// import React from "react";
-// import s from "../Login/Login.module.css";
-// import { reduxForm } from 'redux-form';
-// import { createField, Input } from "../Common/FormsControls/FormsControls";
-// import { maxLengthCreator, required } from "../utils/validators/validators";
-// import { connect } from "react-redux";
-// import { login } from "../../Redux/auth-reduser";
-// // import { Link, redirect } from 'react-router-dom';
-// import { useNavigate } from "react-router-dom";
-// import style from "./../Common/FormsControls/FormsControls.module.css"
+import React from "react";
+import s from "../Login/Login.module.css";
+import { reduxForm } from 'redux-form';
+import { createField, Input } from "../Common/FormsControls/FormsControls";
+import { maxLengthCreator, required } from "../utils/validators/validators";
+import { connect } from "react-redux";
+import { login } from "../../Redux/auth-reduser";
+// import { Link, redirect } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import style from "./../Common/FormsControls/FormsControls.module.css"
 
 
 
-// const LoginForm = (props) => {
-// 	let maxLength30 = maxLengthCreator(30);
+const LoginForm = (props) => {
+	let maxLength30 = maxLengthCreator(30);
 
-// 	return (<>
-// 		<div className={s.log}>
-// 			<p>
-// 				Email: sashachebyrasha16@gmail.com
-// 				<br/>
-// 				Password: sasha1604
-// 			</p>
-// 		</div>
-		
-// 		<form onSubmit={props.handleSubmit}>
-// 			{createField("Email", "email", [required, maxLength30], Input)}
-// 			{createField("Passwosd", "password", [required, maxLength30], Input, { type: "password" })}
-// 			<div className={s.remme}>
-// 				{createField(null, "rememberMe", [], Input, { type: "checkbox" }, "Remember me")}
-// 			</div>
+	return (<>
+		<div className={s.log}>
+			<p>
+				Email: sashachebyrasha16@gmail.com
+				<br />
+				Password: sasha1604
+			</p>
+		</div>
 
-// 			{/* 	<div><Field placeholder="Email" name={"email"} component={Input} validate={[required, maxLength30]} /></div>
-// 			<div><Field placeholder="Passwosd" name={"password"} component={Input} validate={[required, maxLength30]} type={"password"} /></div>
-// 			<div className={s.remme}><Field type={"Checkbox"} name={"rememberMe"} component={Input} />Remember me</div> */}
+		<form onSubmit={props.handleSubmit}>
+			{createField("Email", "email", [required, maxLength30], Input)}
+			{createField("Passwosd", "password", [required, maxLength30], Input, { type: "password" })}
+			<div className={s.remme}>
+				{createField(null, "rememberMe", [], Input, { type: "checkbox" }, "Remember me")}
+			</div>
 
-// 			{props.error &&
-// 				<div className={style.formSummaryError}>
-// 					{props.error}
-// 				</div>
-// 			}
-// 			<div className={s.btn}><button>Login</button></div>
-// 		</form>
-// 	</>
-// 	)
-// }
+			{/* 	<div><Field placeholder="Email" name={"email"} component={Input} validate={[required, maxLength30]} /></div>
+			<div><Field placeholder="Passwosd" name={"password"} component={Input} validate={[required, maxLength30]} type={"password"} /></div>
+			<div className={s.remme}><Field type={"Checkbox"} name={"rememberMe"} component={Input} />Remember me</div> */}
 
-// const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm)
+			{props.error &&
+				<div className={style.formSummaryError}>
+					{props.error}
+				</div>
+			}
+			<div className={s.btn}><button>Login</button></div>
+		</form>
+	</>
+	)
+}
 
-// const Login = (props) => {
-// 	const onSubmit = (formData) => {
-// 		props.login(formData.email, formData.password, formData.rememberMe)
-// 		// console.log(props.login(formData.email, formData.password, formData.rememberMe))
-// 	}
+const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm)
 
-// 	let navigate = useNavigate();
-// 	if (props.isAuth) {
-// 		return (
-// 			// redirect("/profile") 
-// 			navigate("/profile")
-// 			// <Link to={"/profile"} />
-// 		)
-// 	}
+const Login = (props) => {
+	const onSubmit = (formData) => {
+		props.login(formData.email, formData.password, formData.rememberMe)
+		// console.log(props.login(formData.email, formData.password, formData.rememberMe))
+	}
 
-// 	return (
-// 		<div className={s.log}>
-// 			<h1>Please Login</h1>
-// 			<LoginReduxForm onSubmit={onSubmit} />
-// 		</div >
-// 	)
-// }
+	let navigate = useNavigate();
+	if (props.isAuth) {
+		return (
+			// redirect("/profile") 
+			navigate("/profile")
+			// <Link to={"/profile"} />
+		)
+	}
 
-// const mapStateToProps = (state) => ({
-// 	isAuth: state.auth.isAuth
-// })
+	return (
+		<div className={s.log}>
+			<h1>Please Login</h1>
+			<LoginReduxForm onSubmit={onSubmit} />
+		</div >
+	)
+}
 
-// export default connect(mapStateToProps, { login })(Login);
+const mapStateToProps = (state) => ({
+	isAuth: state.auth.isAuth
+})
+
+export default connect(mapStateToProps, { login })(Login);
